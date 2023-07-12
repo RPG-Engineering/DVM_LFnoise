b36becaa2f93e276ca97931a1c9d41b91cb865a2
reps <marco@reps.cc>
2023-07-12 14:12:36 +0200

    Update K182_buffered.py

--- K182_buffered.py
+++ K182_buffered.py
@@ -13,28 +13,28 @@ print("Keithley 182 Firmware:")
 print(DVM.read())
 print("Configuring ...")
 DVM.write("B1X")      # 6.5 digit resolution
-DVM.write("I2X")      # Circular buffer on, length = 1024
-#DVM.write("F1G0X")   # Reading Source: One reading from buffer, reading without prefix
-DVM.write("F1G6X")    # Reading Source: One reading from buffer, reading with time stamp and buffer location
-#DVM.write("F0G0X")   # Reading Source: direct ADC
+DVM.write("I1,1024X") # Linear buffer on, length = 1024
+#DVM.write("G0X")     # Reading without prefix
+DVM.write("G6X")      # Reading with time stamp and buffer location
+DVM.write("F1X")      # Reading Source: One reading from buffer
+#DVM.write("F0X")     # Reading Source: direct ADC
 DVM.write("O1P0N1X")  # Enabled analog filter, disabled dig filter
 DVM.write("R1X")      # 3mV range
 DVM.write("S2X")      # 100ms integration period
 DVM.write("T4X")      # Trigger on X multiple
 
-print("Waiting for the circular buffer to be half full ...")
-#time.sleep(1024/2*.1) # wait for buffer half full
-time.sleep(10) # wait for buffer half full
+print("Giving the buffer a small lead ...")
+time.sleep(5) # wait for buffer half full
 
-last_time = 18
+last_time = 0.0
 
-while True:           # start reading perhaps?
+while True:
     print("Sample from buffer:")
     reading=DVM.read()
     print("VVVVVVVVVVVVV,BufP,second, ms")
     print(reading)
     timestamp=float(reading.split(',')[2].strip())
-    time_difference = last_time-timestamp
+    time_difference = timestamp-last_time
     print("Seconds between this and the last reading:")
     print(time_difference)
     last_time = timestamp
