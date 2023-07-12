bb45262bbebe4517003f688a7045afc578d56f74
reps <marco@reps.cc>
2023-07-12 12:16:03 +0200

    Update K182_buffered.py

--- K182_buffered.py
+++ K182_buffered.py
@@ -4,9 +4,14 @@ from time import time
 rm = visa.ResourceManager()
 
 DVM = rm.open_resource("TCPIP::192.168.0.88::GPIB0,8")
-DVM.timeout = None
+DVM.timeout = 10000   # in ms
 
 DVM.write("DCL")      # clear to default settings
+DVM.write("REN")      # Remote mode
+DVM.write("U2")      # Get firmware test
+print("Keithley 182 Firmware:")
+print(read_data("X"))
+
 DVM.write("B1X")      # 6.5 digit resolution
 DVM.write("I2X")      # Circular buffer on, length = 1024
 #DVM.write("F1G0X")   # Reading Source: One reading from buffer, reading without prefix
